d9ee2c2e93603b82f5a0f9a107d7ca9070a53ff6
Rolly <rollycyber@gmail.com>
2022-12-09 15:32:51 +0700

    route_baru

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\Master\PegawaiController;
 use App\Http\Controllers\Api\Master\TtdbalaiController;
+use App\Http\Controllers\Api\Master\PpkController;
+use App\Http\Controllers\Api\Proses\Surattugas\SurattugasController;
+use App\Http\Controllers\Api\Proses\Surattugas\SurattugasdetailController;
+use App\Http\Controllers\Api\Proses\Suratkeluar\SuratkeluarController;
 use App\Http\Controllers\Api\AuthController;
 
 
@@ -19,40 +23,41 @@ Route::delete("/pegawai/{pegawai}", [PegawaiController::class, "destroy"])->midd
 
 //route ttdbalai
 Route::get("/ttdbalai", [TtdbalaiController::class, "index"])->middleware('auth:sanctum');
-Route::get("/ttdbalai", [App\Http\Controllers\Api\Master\TtdbalaiController::class, "index"]);
-Route::post("/ttdbalai", [App\Http\Controllers\Api\Master\TtdbalaiController::class, "store"]);
-Route::get("/ttdbalai/{ttdbalai}", [App\Http\Controllers\Api\Master\TtdbalaiController::class, "show"]);
-Route::put("/ttdbalai/{ttdbalai}", [App\Http\Controllers\Api\Master\TtdbalaiController::class, "update"]);
-Route::delete("/ttdbalai/{ttdbalai}", [App\Http\Controllers\Api\Master\TtdbalaiController::class, "destroy"]);
+Route::post("/ttdbalai", [TtdbalaiController::class, "store"])->middleware('auth:sanctum');
+Route::get("/ttdbalai", [TtdbalaiController::class, "show"])->middleware('auth:sanctum');
+Route::put("/ttdbalai/{ttdbalai}", [TtdbalaiController::class, "update"])->middleware('auth:sanctum');
+Route::delete("/ttdbalai/{ttdbalai}", [TtdbalaiController::class, "destroy"])->middleware('auth:sanctum');
 
 //route ppk
-Route::get("/ppk", [App\Http\Controllers\Api\Master\PpkController::class, "index"]);
-Route::post("/ppk", [App\Http\Controllers\Api\Master\PpkController::class, "store"]);
-Route::get("/ppk/{ppk}", [App\Http\Controllers\Api\Master\PpkController::class, "show"]);
-Route::put("/ppk/{ppk}", [App\Http\Controllers\Api\Master\PpkController::class, "update"]);
-Route::delete("/ppk/{ppk}", [App\Http\Controllers\Api\Master\PpkController::class, "destroy"]);
+Route::get("/ppk", [PpkController::class, "index"])->middleware('auth:sanctum');
+Route::post("/ppk", [PpkController::class, "store"])->middleware('auth:sanctum');
+Route::get("/ppk/{ppk}", [PpkController::class, "show"])->middleware('auth:sanctum');
+Route::put("/ppk/{ppk}", [PpkController::class, "update"])->middleware('auth:sanctum');
+Route::delete("/ppk/{ppk}", [PpkController::class, "destroy"])->middleware('auth:sanctum');
+
 
 
 //route surat tugas
-Route::get("/sutug", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasController::class, "index"]);
-Route::post("/sutug", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasController::class, "store"]);
-Route::get("/sutug/{sutug}", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasController::class, "show"]);
-Route::put("/sutug/{sutug}", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasController::class, "update"]);
-Route::delete("/sutug/{sutug}", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasController::class, "destroy"]);
+Route::get("/sutug", [SurattugasController::class, "index"])->middleware('auth:sanctum');
+Route::post("/sutug", [SurattugasController::class, "store"])->middleware('auth:sanctum');
+Route::get("/sutug/{sutug}", [SurattugasController::class, "show"])->middleware('auth:sanctum');
+Route::put("/sutug/{sutug}", [SurattugasController::class, "update"])->middleware('auth:sanctum');
+Route::delete("/sutug/{sutug}", [SurattugasController::class, "destroy"])->middleware('auth:sanctum');
 
 //route surat tugas detail
-Route::get("/sutugdetail", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasdetailController::class, "index"]);
-Route::post("/sutugdetail", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasdetailController::class, "store"]);
-Route::get("/sutugdetail/{sutugdetail}", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasdetailController::class, "show"]);
-Route::put("/sutugdetail/{sutugdetail}", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasdetailController::class, "update"]);
-Route::delete("/sutugdetail/{sutugdetail}", [App\Http\Controllers\Api\Proses\Surattugas\SurattugasdetailController::class, "destroy"]);
+Route::get("/sutugdetail", [SurattugasdetailController::class, "index"])->middleware('auth:sanctum');
+Route::post("/sutugdetail", [SurattugasdetailController::class, "store"])->middleware('auth:sanctum');
+Route::get("/sutugdetail/{sutugdetail}", [SurattugasdetailController::class, "show"])->middleware('auth:sanctum');
+Route::put("/sutugdetail/{sutugdetail}", [SurattugasdetailController::class, "update"])->middleware('auth:sanctum');
+Route::delete("/sutugdetail/{sutugdetail}", [SurattugasdetailController::class, "destroy"])->middleware('auth:sanctum');
+
 
 //route surat keluar
-Route::get("/sukel", [App\Http\Controllers\Api\Proses\Suratkeluar\SuratkeluarController::class, "index"]);
-Route::post("/sukel", [App\Http\Controllers\Api\Proses\Suratkeluar\SuratkeluarController::class, "store"]);
-Route::get("/sukel/{sukel}", [App\Http\Controllers\Api\Proses\Suratkeluar\SuratkeluarController::class, "show"]);
-Route::put("/sukel/{sukel}", [App\Http\Controllers\Api\Proses\Suratkeluar\SuratkeluarController::class, "update"]);
-Route::delete("/sukel/{sukel}", [App\Http\Controllers\Api\Proses\Suratkeluar\SuratkeluarController::class, "destroy"]);
+Route::get("/sukel", [SuratkeluarController::class, "index"])->middleware('auth:sanctum');
+Route::post("/sukel", [SuratkeluarController::class, "store"])->middleware('auth:sanctum');
+Route::get("/sukel/{sukel}", [SuratkeluarController::class, "show"])->middleware('auth:sanctum');
+Route::put("/sukel/{sukel}", [SuratkeluarController::class, "update"])->middleware('auth:sanctum');
+Route::delete("/sukel/{sukel}", [SuratkeluarController::class, "destroy"])->middleware('auth:sanctum');
 
 
 
